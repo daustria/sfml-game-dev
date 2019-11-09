47b0e0abd571bc8aa161c6a33bee30a953b9296b
Dominic Austria <dgaustri@uwaterloo.ca>
2019-11-09 10:13:40 -0600

    begin creating enemy class

--- ch1/enemy.cc
+++ ch1/enemy.cc
@@ -1,5 +1,10 @@
 #include "enemy.h"
 
-Enemy::Enemy(const int & x, const int & y, const sf::Vector2f & mv): movement(mv) {}
+Enemy::Enemy(const int & x, const int & y, const sf::Vector2f & mv): movement(mv)
+{
+	shape.setRadius(5.f);
+	shape.setPosition(x, y);
+	shape.setFillColor(sf::Color::Red);
 
+}
 
